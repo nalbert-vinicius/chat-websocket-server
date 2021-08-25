40a221370d5269dc2abb21c434a1a9792d255cfa
Nalbert, Vinicius <rullaxfox@gmail.com>
2021-08-25 09:02:42 -0300

    ajustes no envio da mensagem para sala privada

--- server.js
+++ server.js
@@ -13,19 +13,19 @@ io.on('connection', (socket) =>{
 
     socket.on('criarSala', (data) =>{
         socket.join(data.nomeSala)
-        console.log(data);
     })
 
     socket.on('entrarSala', (data) =>{
         socket.join(data.nomeSala);
     })
 
-    socket.on('message', (msg) =>{
+    socket.on('message', (nomeSala, msg) =>{
         console.log(msg);
         //emite de forma global - broadcast
         //io.emit('message', msg);
 
-        //emite apenas para o socket atual
+        //emite apenas para a sala recebida por parametro
+        socket.in(nomeSala.id).emit('message', msg)
         socket.emit('message', msg);
     })
 
